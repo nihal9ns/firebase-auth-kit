2e1194460eeb747a5e9db6038283b0a195ab5cbc
Nihal <nihal9ns@gmail.com>
2019-04-05 19:49:50 +0530

    DONE AND DUSTED

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import firebase from "firebase";
+import "./App.css";
+
+const config = {
+  apiKey: "AIzaSyAkT2F06bi0jMFK8vuOf5d1HCZR1_ywO8o",
+  authDomain: "facebook-auth-9054d.firebaseapp.com",
+  databaseURL: "https://facebook-auth-9054d.firebaseio.com",
+  projectId: "facebook-auth-9054d",
+  storageBucket: "facebook-auth-9054d.appspot.com",
+  messagingSenderId: "654015793385"
+};
+
+firebase.initializeApp(config);
+
+const auth = firebase.auth;
+const provider = new firebase.auth.FacebookAuthProvider();
 
 class App extends Component {
+  state = {
+    user: null
+  };
+
+  login = () => {
+    auth()
+      .signInWithPopup(provider)
+      .then(({ user }) => {
+        this.setState({ user }, () => {
+          const data = user.providerData[0];
+          const db = firebase.firestore();
+          db.collection("user").add({
+            displayName: data.displayName,
+            email: data.email,
+            phoneNumber: data.phoneNumber,
+            photoURL: data.photoURL,
+            providerId: data.providerId,
+            refreshToken: user.refreshToken,
+            uid: data.uid
+          });
+        });
+      });
+  };
+
+  logout = () => {
+    auth()
+      .signOut()
+      .then(() => {
+        this.setState({ user: null });
+      });
+  };
+
   render() {
+    const { user } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>{user ? `Hi, ${user.displayName}!` : "Hi!"}</p>
+        <button onClick={this.login}>Login with Facebook</button>
+
+        <button onClick={this.logout}>Logout</button>
       </div>
     );
   }
