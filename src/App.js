f3ce736657fd1da69a3c61972664af8dbfb9660c
Nihal <nihal9ns@gmail.com>
2019-04-22 16:49:23 +0530

    added google auth

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ const config = {
 firebase.initializeApp(config);
 
 const auth = firebase.auth;
-const provider = new firebase.auth.FacebookAuthProvider();
 
 class App extends Component {
   state = {
     user: null
   };
 
-  login = () => {
+  facebookLogin = () => {
+    const provider = new firebase.auth.FacebookAuthProvider();
     auth()
       .signInWithPopup(provider)
       .then(({ user }) => {
@@ -41,11 +41,83 @@ class App extends Component {
       });
   };
 
+  googleLogin = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    auth().signInWithPopup(provider);
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        // var token = result.credential.accessToken;
+        // console.log("token : ", token);
+        // The signed-in user info.
+        var user = result.user;
+        // console.log("user : ", user);
+        this.setState({ user }, () => {
+          const data = user.providerData[0];
+          const db = firebase.firestore();
+          db.collection("user").add({
+            displayName: data.displayName,
+            email: data.email,
+            phoneNumber: data.phoneNumber,
+            photoURL: data.photoURL,
+            providerId: data.providerId,
+            refreshToken: user.refreshToken,
+            uid: data.uid
+          });
+        });
+      })
+      .catch(error => {
+        // Handle Errors here.
+        // var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        // var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        // var credential = error.credential;
+        alert("Error : ", errorMessage);
+      });
+  };
+
+  signUpWithEmailPassword = () => {
+    const email = this.refs.email.value;
+    const password = this.refs.password.value;
+    console.log("email : ", email);
+    console.log("password : ", password);
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
+        // var errorCode = error.code;
+        var errorMessage = error.message;
+        alert("Error : ", errorMessage);
+      });
+  };
+
+  loginWithEmailPassword = () => {
+    const email = this.refs.email.value;
+    const password = this.refs.password.value;
+    console.log("email : ", email);
+    console.log("password : ", password);
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
+        // var errorCode = error.code;
+        var errorMessage = error.message;
+        alert("Error : ", errorMessage);
+      });
+  };
+
   logout = () => {
     auth()
       .signOut()
       .then(() => {
         this.setState({ user: null });
+      })
+      .catch(error => {
+        alert("Error : ", error);
       });
   };
 
@@ -54,8 +126,22 @@ class App extends Component {
     return (
       <div className="App">
         <p>{user ? `Hi, ${user.displayName}!` : "Hi!"}</p>
-        <button onClick={this.login}>Login with Facebook</button>
-
+        <button onClick={this.facebookLogin}>Login with Facebook</button> <br />
+        <br />
+        <button onClick={this.googleLogin}>Login with Google</button> <br />
+        <br />
+        <form>
+          <input type="text" ref="email" placeholder="email" /> <br />
+          <br />
+          <input type="password" ref="password" placeholder="password" />
+          <br />
+          <br />
+          <button onClick={this.signUpWithEmailPassword.bind(this)}>
+            Sign Up
+          </button>
+          <br />
+          <br />
+        </form>
         <button onClick={this.logout}>Logout</button>
       </div>
     );
